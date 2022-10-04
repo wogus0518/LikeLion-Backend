99d579892c105bc2213b67618abd1c57efd82e87
JaeHyun <wogus0518@naver.com>
2022-10-04 16:47:32 +0900

    UPDATE date221004

---
+++ Class/src/week3/date221004/ArrayTest.java
@@ -0,0 +1,13 @@
+package week3.date221004;
+
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i+1;
+        }
+        System.out.println("arr = " + Arrays.toString(arr));
+    }
+}
