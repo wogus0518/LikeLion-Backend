d0014d74150b45689f82f24bbc131cb8c510e3f5
JaeHyun <wogus0518@naver.com>
2022-10-06 09:35:56 +0900

    Add ListExcerciseMain

---
+++ Class/src/week3/date221006/collection/ListExcerciseMain.java
@@ -0,0 +1,15 @@
+package week3.date221006.collection;
+
+import java.util.List;
+
+public class ListExcerciseMain {
+    public static void main(String[] args) {
+        ListExercise listExercise = new ListExercise();
+        List<String> students = listExercise.getStudents();
+
+        for (String student : students) {
+            System.out.println(student);
+        }
+        System.out.println(students.size());
+    }
+}
---
+++ Class/src/week3/date221006/collection/ListExercise.java
@@ -0,0 +1,21 @@
+package week3.date221006.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+    private List<String> students;
+
+    public ListExercise() {
+        students = new ArrayList<>();
+        students.add("홍길동1");
+        students.add("홍길동2");
+        students.add("홍길동3");
+        students.add("홍길동4");
+        students.add("홍길동5");
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
+}
