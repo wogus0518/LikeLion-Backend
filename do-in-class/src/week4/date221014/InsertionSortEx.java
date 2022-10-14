6cca42cb42800e61bfd49eb54a28cd7d09282352
JaeHyun <wogus0518@naver.com>
2022-10-14 09:33:05 +0900

    수업중

--- do-in-class/src/week4/date221014/InsertionSortEx.java
+++ do-in-class/src/week4/date221014/InsertionSortEx.java
@@ -3,12 +3,10 @@ package week4.date221014;
 import java.util.Arrays;
 
 public class InsertionSortEx {
-    public static void main(String[] args) {
-        int[] arr = new int[]{8, 5, 6, 2, 4};
 
+    public static int[] sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int temp = arr[i];
-
             for (int j = i - 1; j >= 0; j--) {
                 if (arr[j] > temp) {
                     arr[j + 1] = arr[j];
@@ -16,6 +14,11 @@ public class InsertionSortEx {
                 }
             }
         }
-        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{8, 5, 6, 2, 4};
+        System.out.println(Arrays.toString(sort(arr)));
     }
 }
