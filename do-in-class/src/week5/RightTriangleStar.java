82ff1a85f43d11b43091eb570daec7f785592297
JaeHyun <wogus0518@naver.com>
2022-10-17 09:07:41 +0900

    수업중

---
+++ do-in-class/src/week5/RightTriangleStar.java
@@ -0,0 +1,17 @@
+package week5;
+
+public class RightTriangleStar {
+    public void stamp(int length) {
+        for (int i = 1; i <= length; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        RightTriangleStar stamper = new RightTriangleStar();
+        stamper.stamp(4);
+    }
+}
